f4b853a944dc22717fb4ad96d98f7d50f6c70ad1
ladyjemea <ladyjemea@gmail.com>
2025-02-18 17:13:26 +0100

    adhoc requests

--- dagster_university/jobs/__init__.py
+++ dagster_university/jobs/__init__.py
@@ -4,10 +4,12 @@ from ..partitions import monthly_partition, weekly_partition
 
 trips_by_week = AssetSelection.assets("trips_by_week")
 
+adhoc_request = AssetSelection.assets(["adhoc_request"])
+
 trip_update_job = define_asset_job(
     name="trip_update_job",
     partitions_def=monthly_partition,  # partitions added here
-    selection=AssetSelection.all() - trips_by_week,
+    selection=AssetSelection.all() - trips_by_week - adhoc_request,
 )
 
 
@@ -15,4 +17,10 @@ weekly_update_job = define_asset_job(
     name="weekly_update_job",
     partitions_def=weekly_partition,
     selection=trips_by_week,
+)
+
+
+adhoc_request_job = define_asset_job(
+    name="adhoc_request_job",
+    selection=adhoc_request,
 )
