dd36cfe47fccbdf7a636f5a57ba59d0ec8e4c62f
ladyjemea <ladyjemea@gmail.com>
2025-02-24 16:48:50 +0100

    added sensors

--- dagster_university/jobs/__init__.py
+++ dagster_university/jobs/__init__.py
@@ -4,7 +4,16 @@ from ..partitions import monthly_partition, weekly_partition
 
 trips_by_week = AssetSelection.assets("trips_by_week")
 
-adhoc_request = AssetSelection.assets(["adhoc_request"])
+adhoc_request = AssetSelection.assets(
+    "dagster_university/assets/requests.adhoc_request"
+)
+
+
+adhoc_request_job = define_asset_job(
+    name="adhoc_request_job",
+    selection=adhoc_request,
+)
+
 
 trip_update_job = define_asset_job(
     name="trip_update_job",
@@ -17,10 +26,4 @@ weekly_update_job = define_asset_job(
     name="weekly_update_job",
     partitions_def=weekly_partition,
     selection=trips_by_week,
-)
-
-
-adhoc_request_job = define_asset_job(
-    name="adhoc_request_job",
-    selection=adhoc_request,
 )
